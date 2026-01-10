974c187bf3be5baad61e0cba4db0963f9e41896c
maddsua <maddsuadev@pm.me>
2026-01-10 04:37:02 +0100

    Require non-localhost remotes to always use https

--- cli/commands/set_remote_url.go
+++ cli/commands/set_remote_url.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"net"
 	"net/url"
 
 	"github.com/maddsua/syncctl/cli/config"
@@ -19,6 +20,37 @@ func SetRemoteUrl(inputURL string, cfg *config.Config) error {
 	case "http", "https":
 
 		fmt.Println("Note: Assuming S4 remote url")
+
+		var didYouBringProtection = func() error {
+
+			//	yep he did
+			if remoteURL.Scheme == "https" {
+				return nil
+			}
+
+			//	tbh I wanted to use some corny variable names here as well,
+			//	but then it sucks ass to read the code
+			hostname := remoteURL.Host
+			if val, _, err := net.SplitHostPort(hostname); err == nil {
+				hostname = val
+			}
+
+			//	no need for protection if there's only one person doing it
+			if hostname == "localhost" || net.ParseIP(hostname).IsLoopback() {
+				return nil
+			}
+
+			//	it's mostly idiot-proofing, because if you allow for it - some idiot
+			//	will inevitably stick his dingus into the power outlet
+			//	and then blame you for all the fireworks
+			fmt.Print("\n!!!    Nah, hollon pardner!    !!!\n\n")
+			fmt.Println("You're a big boy and shi but don't be silly and use protection")
+			return fmt.Errorf("TLS-less connections are not allowed outside of localhost")
+		}
+
+		if err := didYouBringProtection(); err != nil {
+			return err
+		}
 
 		baseURL := url.URL{
 			Scheme: remoteURL.Scheme,
