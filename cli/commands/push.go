0fd0bb74744668cb7f799879ad9b4f9cfa304400
maddsua <maddsuadev@pm.me>
2026-01-10 20:02:37 +0100

    Remove mtime check on push
    I'm not overwriting and entire ass file just because it's mtime doesn't
    match

--- cli/commands/push.go
+++ cli/commands/push.go
@@ -89,8 +89,7 @@ func pushEntry(ctx context.Context, client s4.StorageClient, name, remotePath st
 
 		case syncctl.ResolveOverwrite:
 
-			//	todo: fix timestamp check failing
-			if remoteEntry.SHA256 == hash && remoteEntry.Modified.Equal(stat.ModTime()) {
+			if remoteEntry.SHA256 == hash {
 				fmt.Printf("--> Up to date '%s'\n", remotePath)
 				return nil
 			}
